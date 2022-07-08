fadb78feeff1411b5176eaba363391e95a95f8a9
anraras <oleg.fort27@gmail.com>
2022-07-08 21:48:36 +0300

    update: do test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    n=n.toString().split('').reverse().join('').replace("-", '')
+    
+    for (let i =0; n[0] === '0';) {
+        n=n.slice(1)
+
+    }
+    return n
   
 }
+
+
